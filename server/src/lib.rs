64a763ddfb55dbb3ef1677feae257766232a95c7
yexiyue <yexiyue666@qq.com>
2023-09-03 16:18:17 +0800

    feat: 静态文件服务和文件上传

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,14 +1,27 @@
-use axum::{Router, Server, response::IntoResponse, routing::get};
+use axum::{
+    response::IntoResponse,
+    routing::{get, get_service},
+    Router, Server,
+};
+use error::ServerError;
 use once_cell::sync::Lazy;
 use std::net::SocketAddr;
 use tracing::info;
 
+
 pub mod database;
 pub mod error;
+pub mod upload;
+
+type ServeResult<T> = Result<T, ServerError>;
+
 static ADDR: Lazy<SocketAddr> = Lazy::new(|| "127.0.0.1:4060".parse().unwrap());
 pub async fn start() {
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", get(hello_world));
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .merge(upload::upload_router())
+        .fallback_service(static_serve());
     let server = Server::bind(&ADDR).serve(app.into_make_service());
     info!("Server listening on http://{:?}", server.local_addr());
     server.await.unwrap();
@@ -16,4 +29,11 @@ pub async fn start() {
 
 pub async fn hello_world() -> impl IntoResponse {
     "hello world"
+}
+
+pub fn static_serve() -> Router {
+    Router::new().nest_service(
+        "/",
+        get_service(tower_http::services::ServeDir::new("public")),
+    )
 }
